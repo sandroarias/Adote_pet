f36c7c1be998a298bed03a31d22fbd1714f5e5ae
sandro ospina arias <sandro901217@gmail.com>
2023-01-12 15:05:53 -0300

    Primeiro comit

---
+++ divulgar/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Pet, Raca, Tag
+
+
+@login_required
+def novo_pet(request):
+    if request.method == "GET":
+        tags = Tag.objects.all()
+        racas = Raca.objects.all()
+        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+    elif request.method == "POST":
+        foto = request.FILES.get('foto')
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+        tags = request.POST.getlist('tags')
+        raca = request.POST.get('raca')
+
+        pet = Pet(
+            usuario=request.user,
+            foto=foto,
+            nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca,
+        )
+
+        pet.save()
+
+        for tag_id in tags:
+            tag = Tag.objects.get(id=tag_id)
+            pet.tags.add(tag)
+
+        pet.save()
